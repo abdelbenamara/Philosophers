dd2b36d1e90cd945e0a9108028399e30ed6361db
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-17 00:23:43 +0100

    Updated philo

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:12:58 by abenamar          #+#    #+#             */
-/*   Updated: 2023/11/12 14:26:24 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/11/16 22:13:13 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static uint8_t	ft_is_natural_number(const char *nptr)
 	return (1);
 }
 
-static uint8_t	ft_setup_arg(size_t *nb, const char *nptr)
+static uint8_t	ft_arg_setup(size_t *nb, const char *nptr)
 {
 	size_t	i;
 	char	*s;
@@ -51,7 +51,7 @@ static uint8_t	ft_setup_arg(size_t *nb, const char *nptr)
 	return (1);
 }
 
-static uint8_t	ft_init_args(t_args *args, char **strs)
+static uint8_t	ft_args_init(t_args *args, char **strs)
 {
 	size_t	i;
 
@@ -62,10 +62,10 @@ static uint8_t	ft_init_args(t_args *args, char **strs)
 			return (ft_pstderr(__ERR_1), 0);
 		++i;
 	}
-	if (!ft_setup_arg(&args->number_of_philosophers, strs[0])
-		|| !ft_setup_arg(&args->time_to_die, strs[1])
-		|| !ft_setup_arg(&args->time_to_eat, strs[2])
-		|| !ft_setup_arg(&args->time_to_sleep, strs[3]))
+	if (!ft_arg_setup(&args->number_of_philosophers, strs[0])
+		|| !ft_arg_setup(&args->time_to_die, strs[1])
+		|| !ft_arg_setup(&args->time_to_eat, strs[2])
+		|| !ft_arg_setup(&args->time_to_sleep, strs[3]))
 		return (0);
 	if (args->number_of_philosophers > SIZE_MAX / sizeof(t_philo))
 		return (ft_pstderr(__ERR_3), 0);
@@ -75,7 +75,7 @@ static uint8_t	ft_init_args(t_args *args, char **strs)
 	args->number_of_times_each_philosopher_must_eat = malloc(sizeof(size_t));
 	if (!(args->number_of_times_each_philosopher_must_eat))
 		return (ft_pstderr(__ERR_4), 0);
-	if (!ft_setup_arg(args->number_of_times_each_philosopher_must_eat, strs[4]))
+	if (!ft_arg_setup(args->number_of_times_each_philosopher_must_eat, strs[4]))
 		return (free(args->number_of_times_each_philosopher_must_eat), 0);
 	return (1);
 }
@@ -86,7 +86,7 @@ int	main(int ac, char **av)
 
 	if (ac < 5 || ac > 6)
 		return (ft_pstderr(__USAGE), 2);
-	if (!ft_init_args(&args, av + 1))
+	if (!ft_args_init(&args, av + 1))
 		return (EXIT_FAILURE);
 	printf(__DEBUG "number_of_philosophers: %ld\n",
 		args.number_of_philosophers);
