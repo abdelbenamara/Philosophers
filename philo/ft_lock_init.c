b3f0b4a11a8b8a312b863f7093aa03ac127a85e3
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-17 00:23:05 +0100

    Added ft_lock_init

---
+++ philo/ft_lock_init.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lock_init.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 21:48:40 by abenamar          #+#    #+#             */
+/*   Updated: 2023/11/16 21:49:17 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+t_lock	*ft_lock_init(size_t size)
+{
+	t_lock	*lock;
+	size_t	i;
+
+	lock = malloc(size * sizeof(t_lock));
+	if (!lock)
+		return (ft_pstderr(__ERR_4), NULL);
+	i = 0;
+	while (i < size)
+	{
+		lock[i].acquired = 0;
+		if (pthread_mutex_init(&lock[i].mutex, NULL))
+			return (ft_lock_free(lock, i + 1), ft_pstderr(__ERR_5), NULL);
+		++i;
+	}
+	return (lock);
+}
