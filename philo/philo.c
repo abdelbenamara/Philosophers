d92cac962f277b9c933b444d5cea27ba8820255f
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-19 11:50:04 +0100

    Updated philo

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:12:58 by abenamar          #+#    #+#             */
-/*   Updated: 2023/11/16 22:13:13 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/11/19 11:27:25 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ static uint8_t	ft_arg_setup(size_t *nb, const char *nptr)
 	if (!s)
 		return (0);
 	if (ft_strncmp(s, nptr, ft_strlen(s)))
-		return (ft_pstderr(__ERR_2), free(s), 0);
+		return (free(s), 0);
 	free(s);
 	*nb = i;
 	return (1);
@@ -66,7 +66,7 @@ static uint8_t	ft_args_init(t_args *args, char **strs)
 		|| !ft_arg_setup(&args->time_to_die, strs[1])
 		|| !ft_arg_setup(&args->time_to_eat, strs[2])
 		|| !ft_arg_setup(&args->time_to_sleep, strs[3]))
-		return (0);
+		return (ft_pstderr(__ERR_2), 0);
 	if (args->number_of_philosophers > SIZE_MAX / sizeof(t_philo))
 		return (ft_pstderr(__ERR_3), 0);
 	args->number_of_times_each_philosopher_must_eat = NULL;
@@ -88,17 +88,6 @@ int	main(int ac, char **av)
 		return (ft_pstderr(__USAGE), 2);
 	if (!ft_args_init(&args, av + 1))
 		return (EXIT_FAILURE);
-	printf(__DEBUG "number_of_philosophers: %ld\n",
-		args.number_of_philosophers);
-	printf(__DEBUG "time_to_die (ms): %ld\n", args.time_to_die);
-	printf(__DEBUG "time_to_eat (ms): %ld\n", args.time_to_eat);
-	printf(__DEBUG "time_to_sleep (ms): %ld\n", args.time_to_sleep);
-	if (args.number_of_times_each_philosopher_must_eat)
-		printf(__DEBUG "number_of_times_each_philosopher_must_eat: %ld\n",
-			*(args.number_of_times_each_philosopher_must_eat));
-	else
-		printf(__DEBUG "number_of_times_each_philosopher_must_eat: %p\n",
-			args.number_of_times_each_philosopher_must_eat);
 	if (!ft_simulate(args))
 		return (free(args.number_of_times_each_philosopher_must_eat), \
 			EXIT_FAILURE);
