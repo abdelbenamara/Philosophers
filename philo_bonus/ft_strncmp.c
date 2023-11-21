9f21e3ef5fb949a6946c92162fee542ec3f2e14a
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-21 02:15:18 +0100

    Added ft_strncmp

---
+++ philo_bonus/ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 15:20:06 by abenamar          #+#    #+#             */
+/*   Updated: 2023/11/19 12:11:35 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	const unsigned char	*r1 = (unsigned char *) s1;
+	const unsigned char	*r2 = (unsigned char *) s2;
+
+	if (!n)
+		return (0);
+	while (--n && *r1 && *r2)
+	{
+		if (*r1 != *r2)
+			break ;
+		++r1;
+		++r2;
+	}
+	return (*r1 - *r2);
+}
