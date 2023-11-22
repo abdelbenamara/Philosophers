4fae26bf7ca832f89c1dc8fe46a7d5aad0295d7e
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-22 13:23:26 +0100

    Updated ft_simulate

--- philo_bonus/ft_simulate.c
+++ philo_bonus/ft_simulate.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 19:54:11 by abenamar          #+#    #+#             */
-/*   Updated: 2023/11/21 02:09:08 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/11/22 13:15:15 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-static void	ft_philo_setup(t_philo *philo, t_args args)
+static void	ft_philo_setup(t_philo *philo, t_args args, sem_t **sem)
 {
 	size_t	i;
 
@@ -27,63 +27,73 @@ static void	ft_philo_setup(t_philo *philo, t_args args)
 			= args.number_of_times_each_philosopher_must_eat;
 		philo[i].meal_count = 0;
 		philo[i].state = thinking;
+		philo[i].stop = sem[0];
+		philo[i].forks = sem[1];
+		philo[i].meal_goal = sem[2];
+		sem_post(philo->forks);
 		++i;
 	}
 }
 
-static uint8_t	ft_fork(t_philo *philo, t_args args, sem_t *forks)
+static uint8_t	ft_philo_fork(t_philo *philo, t_args args, sem_t **sem)
 {
 	size_t	i;
+	size_t	j;
 
 	i = 0;
 	while (i < args.number_of_philosophers)
 	{
 		philo[i].id = fork();
 		if (philo[i].id == -1)
+		{
+			j = 0;
+			while (j < i)
+			{
+				kill(philo[j].id, SIGKILL);
+				waitpid(philo[i].id, NULL, WUNTRACED);
+				++j;
+			}
 			return (0);
+		}
 		if (!philo[i].id)
-			return (ft_routine(philo, i, forks), 1);
-		++i;
-	}
-	i = 0;
-	while (i < args.number_of_philosophers)
-	{
-		sem_post(forks);
+			return (ft_routine(philo, i, sem), 1);
 		++i;
 	}
 	return (1);
 }
 
-static uint8_t	ft_wait(t_philo *philo, t_args args)
+static void	ft_philo_wait(t_philo *philo, t_args args)
 {
 	size_t	i;
 
+	sem_wait(philo->stop);
 	i = 0;
 	while (i < args.number_of_philosophers)
 	{
-		if (waitpid(philo[i].id, NULL, WUNTRACED) == -1)
-			return (0);
+		kill(philo[i].id, SIGKILL);
+		waitpid(philo[i].id, NULL, WUNTRACED);
 		++i;
 	}
-	return (1);
 }
 
-uint8_t	ft_simulate(t_args args, sem_t *forks)
+uint8_t	ft_simulate(t_args args)
 {
+	sem_t	**sem;
 	t_philo	*philo;
 
 	if (!args.number_of_philosophers)
 		return (1);
+	sem = ft_sem_init();
+	if (!sem)
+		return (0);
 	philo = malloc(args.number_of_philosophers * sizeof(t_philo));
 	if (!philo)
-		return (ft_pstderr(__ERR_4), 0);
-	ft_philo_setup(philo, args);
-	if (!ft_fork(philo, args, forks))
-		return (free(philo), sem_close(forks), \
-			ft_pstderr(__ERR_6), ft_wait(philo, args));
-	sem_close(forks);
-	if (!ft_wait(philo, args))
-		return (free(philo), 0);
+		return (ft_sem_free(sem), ft_pstderr(__ERR_4), 0);
+	ft_philo_setup(philo, args, sem);
+	if (!ft_philo_fork(philo, args, sem))
+		return (ft_sem_free(sem), free(philo), ft_pstderr(__ERR_6), 0);
+	ft_philo_wait(philo, args);
+	ft_sem_free(sem);
 	free(philo);
 	return (1);
 }
