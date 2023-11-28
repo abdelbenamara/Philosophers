c1af542e3c8688a560cd52f2a7aad2004652463d
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-28 08:27:22 +0100

    Updated ft_sem_free

--- philo_bonus/ft_sem_free.c
+++ philo_bonus/ft_sem_free.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 21:49:22 by abenamar          #+#    #+#             */
-/*   Updated: 2023/11/22 12:45:30 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/11/27 00:29:10 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ void	ft_sem_free(sem_t **sem)
 	sem_unlink(__SEM_2);
 	sem_close(sem[2]);
 	sem_unlink(__SEM_3);
+	sem_close(sem[3]);
+	sem_unlink(__SEM_4);
 	free(sem);
 }
