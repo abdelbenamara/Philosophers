d09576a5007332782980fb325e4f60de919fc64d
Abdel Benamara <abdelb.contact@gmail.com>
2023-11-28 08:27:25 +0100

    Updated ft_sem_init

--- philo_bonus/ft_sem_init.c
+++ philo_bonus/ft_sem_init.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 21:48:40 by abenamar          #+#    #+#             */
-/*   Updated: 2023/11/22 12:45:52 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/11/28 07:38:41 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ sem_t	**ft_sem_init(void)
 {
 	sem_t	**sem;
 
-	sem = malloc(3 * sizeof(sem_t *));
+	sem = malloc(4 * sizeof(sem_t *));
 	if (!sem)
 		return (ft_pstderr(__ERR_4), NULL);
 	sem[0] = sem_open(__SEM_1, O_RDWR | O_CREAT | O_EXCL, \
@@ -30,6 +30,10 @@ sem_t	**ft_sem_init(void)
 	sem[2] = sem_open(__SEM_3, O_RDWR | O_CREAT | O_EXCL, \
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP, 0);
 	if (sem[2] == SEM_FAILED)
+		return (ft_sem_free(sem), ft_pstderr(__ERR_5), NULL);
+	sem[3] = sem_open(__SEM_4, O_RDWR | O_CREAT | O_EXCL, \
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP, 0);
+	if (sem[3] == SEM_FAILED)
 		return (ft_sem_free(sem), ft_pstderr(__ERR_5), NULL);
 	return (sem);
 }
